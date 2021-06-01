e870bb73fc73c44a14289ea0a05a7d6ff6460979
Artem Kliminskyi <artemklim50@gmail.com>
2021-06-01 18:38:27 +0300

    Add GPLv3 licenses to all source files

--- src/nn/SRCNN.cpp
+++ src/nn/SRCNN.cpp
@@ -1,3 +1,21 @@
+/*
+ * ImageUpscalerQt SRC Neural network
+ * Copyright (C) 2021  Artem Kliminskyi <artemklim50@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "SRCNN.h"
 
 SRCNNImpl::SRCNNImpl(std::array<unsigned short, 3> kernels, std::array<unsigned short, 3> paddings,
