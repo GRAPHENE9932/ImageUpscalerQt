ff123f1e0b73942a85c5b36bd807329fc60942e3
Artem Kliminskyi <graphene64@gmail.com>
2022-01-15 13:49:26 +0200

    Update TaskDesc and add cpp file for it.

---
+++ src/tasks/TaskDesc.cpp
@@ -0,0 +1,200 @@
+/*
+ * ImageUpscalerQt - task descriptions
+ * SPDX-FileCopyrightText: 2021 Artem Kliminskyi, artemklim50@gmail.com
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <QStringList>
+
+#include "TaskDesc.h"
+
+// BEGIN SRCNN
+
+QString SRCNNDesc::to_string() const {
+	// 5-1-9 64-32.
+	return QString("%1-%2-%3 %4-%5").arg(QString::number(kernels[0]),
+										 QString::number(kernels[1]),
+										 QString::number(kernels[2]),
+										 QString::number(channels[1]),
+										 QString::number(channels[2]));
+}
+
+bool SRCNNDesc::from_string(QString str, SRCNNDesc* srcnn) {
+	try {
+		QStringList parts = str.split(' ');
+
+		if (parts.size() != 2)
+			return false;
+
+		std::array<unsigned short, 3> kernels = {0, 0, 0};
+		std::array<unsigned short, 3> paddings = {0, 0, 0};
+		std::array<unsigned short, 4> channels = {1, 0, 0, 1};
+
+		// BEGIN Parse first part (kernels)
+		QStringList splitted_ker = parts[0].split('-');
+
+		if (splitted_ker.size() != 3)
+			return false;
+
+		bool bad_params = false;
+
+		for (unsigned char i = 0; i < 3; i++) {
+			// Parse name.
+			kernels[i] = splitted_ker[i].toUShort();
+			paddings[i] = (kernels[i] - 1) / 2; // Formulas simplified from (w - k + 2p) / s + 1.
+			bad_params |= kernels[i] % 2 != 1;
+		}
+
+		if (bad_params)
+			return false;
+		// END
+
+		// BEGIN Parse second part (channels)
+		QStringList splitted_ch = parts[1].split('-');
+
+		if (splitted_ch.size() != 2)
+			return false;
+
+		for (unsigned char i = 0; i < 2; i++)
+			channels[i + 1] = splitted_ch[i].toUShort();
+		// END
+
+		// Return values.
+		if (srcnn != nullptr) {
+			srcnn->kernels = kernels;
+			srcnn->paddings = paddings;
+			srcnn->channels = channels;
+		}
+		return true;
+	}
+	catch (...) {
+		return false;
+	}
+}
+
+bool SRCNNDesc::operator<(const SRCNNDesc right) {
+	for (unsigned char i = 0; i < 3; i++)
+		if (kernels[i] < right.kernels[i])
+			return true;
+
+	for (unsigned char i = 0; i < 3; i++)
+		if (channels[i] < right.channels[i])
+			return true;
+
+	return false;
+}
+
+bool SRCNNDesc::operator>(const SRCNNDesc right) {
+	return (*this < right || *this == right);
+}
+
+bool SRCNNDesc::operator==(const SRCNNDesc right) {
+	return kernels == right.kernels && channels == right.channels;
+}
+
+// END SRCNN
+
+// BEGIN FSRCNN
+
+QString FSRCNNDesc::to_string() const {
+	QString output;
+
+	for (size_t i = 0; i < kernels.size(); i++) {
+		output += QString::number(kernels[i]);
+		if (i != kernels.size() - 1)
+			output += QChar('-');
+	}
+
+	output += QChar(' ');
+
+	for (size_t i = 1; i < channels.size() - 1; i++) {
+		output += QString::number(channels[i]);
+		if (i != channels.size() - 2)
+			output += QChar('-');
+	}
+
+	return output;
+}
+
+bool FSRCNNDesc::from_string(QString str, FSRCNNDesc* fsrcnn) {
+	try {
+		QStringList parts = str.split(' ');
+
+		if (parts.size() != 2)
+			return false;
+
+		// BEGIN Parse first part (kernels)
+		QStringList splitted_ker = parts[0].split('-');
+
+		// Initialize kernels and paddigns vectors.
+		std::vector<unsigned short> kernels(splitted_ker.size(), 0);
+		std::vector<unsigned short> paddings(splitted_ker.size(), 0);
+
+		bool bad_params = false;
+
+		for (uint8_t i = 0; i < splitted_ker.size(); i++) {
+			kernels[i] = splitted_ker[i].toUShort();
+			if (i == splitted_ker.size() - 1) { // Last element (conv transpos).
+				paddings[i] = (kernels[i] - 3) / 2;
+			}
+			else {
+				paddings[i] = (kernels[i] - 1) / 2; // Formulas simplified from (w - k + 2p) / s + 1.
+				bad_params |= (kernels[i] % 2) != 1;
+			}
+		}
+
+		if (bad_params)
+			return false;
+
+		// END
+
+		// BEGIN Parse second part (channels)
+		QStringList splitted_ch = parts[1].split('-');
+		// Inititalize channels vector.
+		std::vector<unsigned short> channels(splitted_ker.size() + 1, 0);
+		channels[0] = 1;
+		channels[channels.size() - 1] = 1;
+
+		for (uint8_t i = 0; i < splitted_ch.size(); i++)
+			channels[i + 1] = splitted_ch[i].toUShort();
+		// END
+
+		// Return values.
+		if (fsrcnn != nullptr) {
+			fsrcnn->kernels = kernels;
+			fsrcnn->paddings = paddings;
+			fsrcnn->channels = channels;
+		}
+		return true;
+	}
+	catch (...) {
+		return false;
+	}
+}
+
+bool FSRCNNDesc::operator<(const FSRCNNDesc right) {
+	if (kernels.size() < right.kernels.size() || channels.size() < right.channels.size())
+		return true;
+
+	// After previous statement, we can assume that amount of kernels and channels
+	// are equal in both operands.
+	for (unsigned char i = 0; i < kernels.size(); i++)
+		if (kernels[i] < right.kernels[i])
+			return true;
+
+	for (unsigned char i = 0; i < channels.size(); i++)
+		if (channels[i] < right.channels[i])
+			return true;
+
+	return false;
+}
+
+bool FSRCNNDesc::operator>(const FSRCNNDesc right) {
+	return (*this < right || *this == right);
+}
+
+bool FSRCNNDesc::operator==(const FSRCNNDesc right) {
+	return kernels == right.kernels && channels == right.channels;
+}
+
+// END FSRCNN
--- src/tasks/TaskDesc.h
+++ src/tasks/TaskDesc.h
@@ -1,5 +1,5 @@
 /*
- * ImageUpscalerQt - task descriptions
+ * ImageUpscalerQt - task descriptions header
  * SPDX-FileCopyrightText: 2021 Artem Kliminskyi, artemklim50@gmail.com
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
@@ -8,6 +8,8 @@
 
 #include <array>
 #include <vector>
+
+#include <QString>
 
 enum class TaskKind : unsigned char {
 	resize,
@@ -46,47 +48,116 @@ const QString COLOR_SPACE_CONVERSION_NAMES[4] = {
 	"RGB to YCbCr", "YCbCr to RGB", "RGB to YCoCg", "YCoCg to RGB"
 };
 
-struct TaskResizeDesc {
+struct TaskDesc {
+	virtual TaskKind task_kind() const = 0;
+};
+
+struct TaskResizeDesc : TaskDesc {
 	Interpolation interpolation = Interpolation::bilinear;
 	unsigned int x_size = 0, y_size = 0;
 
 	TaskResizeDesc(Interpolation interpolation, unsigned int x_size, unsigned int y_size) :
 		interpolation(interpolation), x_size(x_size), y_size(y_size) {}
+
+	TaskKind task_kind() const override {
+		return TaskKind::resize;
+	}
 };
 
-struct TaskConvertColorSpaceDesc {
+struct TaskConvertColorSpaceDesc : TaskDesc {
 	ColorSpaceConversion color_space_conversion;
 
 	TaskConvertColorSpaceDesc(ColorSpaceConversion color_space_conversion) :
 		color_space_conversion(color_space_conversion) {}
+
+	TaskKind task_kind() const override {
+		return TaskKind::convert_color_space;
+	}
 };
 
-struct TaskSRCNNDesc {
+struct SRCNNDesc {
 	std::array<unsigned short, 3> kernels;
 	std::array<unsigned short, 3> paddings;
 	std::array<unsigned short, 4> channels;
+
+	SRCNNDesc() = default;
+
+	SRCNNDesc(std::array<unsigned short, 3> kernels,
+			  std::array<unsigned short, 3> paddings,
+			  std::array<unsigned short, 4> channels) :
+			  kernels(kernels), paddings(paddings), channels(channels) {}
+
+	QString to_string() const;
+	/// Parse SRCNN. Returns true if parsing is successful.
+	/// Returns false if it is impossible to parse.
+	/// Pass nullptr as pointer for desc to validate if it is valid SRCNN description string.
+	static bool from_string(QString str, SRCNNDesc* desc);
+
+	bool operator< (const SRCNNDesc right);
+	bool operator> (const SRCNNDesc right);
+	bool operator== (const SRCNNDesc right);
+};
+
+struct TaskSRCNNDesc : TaskDesc {
+	SRCNNDesc srcnn_desc;
 	/// Block size of the input image that will be splitted into blocks before the CNN.
 	/// 0 if the input image have not to be splitted.
 	unsigned int block_size;
+
+	TaskSRCNNDesc(SRCNNDesc srcnn_desc, unsigned int block_size) :
+				  srcnn_desc(srcnn_desc), block_size(block_size) {}
 
 	TaskSRCNNDesc(std::array<unsigned short, 3> kernels,
 				  std::array<unsigned short, 3> paddings,
 				  std::array<unsigned short, 4> channels,
 				  unsigned int block_size) :
-				  kernels(kernels), paddings(paddings), channels(channels), block_size(block_size) {}
+				  srcnn_desc(kernels, paddings, channels), block_size(block_size) {}
+
+	TaskKind task_kind() const override {
+		return TaskKind::srcnn;
+	}
 };
 
-struct TaskFSRCNNDesc {
+struct FSRCNNDesc {
 	std::vector<unsigned short> kernels;
 	std::vector<unsigned short> paddings;
 	std::vector<unsigned short> channels;
+
+	FSRCNNDesc() = default;
+
+	FSRCNNDesc(std::vector<unsigned short> kernels,
+			   std::vector<unsigned short> paddings,
+			   std::vector<unsigned short> channels) :
+			   kernels(kernels), paddings(paddings), channels(channels) {}
+
+	QString to_string() const;
+	/// Parse FSRCNN. Returns true if parsing is successful.
+	/// Returns false if it is impossible to parse.
+	/// Pass nullptr as pointer for desc to validate if it is valid FSRCNN description string.
+	static bool from_string(QString str, FSRCNNDesc* desc);
+
+	bool operator< (const FSRCNNDesc right);
+	bool operator> (const FSRCNNDesc right);
+	bool operator== (const FSRCNNDesc right);
+};
+
+struct TaskFSRCNNDesc : TaskDesc {
+	FSRCNNDesc fsrcnn_desc;
 	/// Block size of the input image that will be splitted into blocks before the CNN.
 	/// 0 if the input image have not to be splitted.
 	unsigned int block_size;
+
+	TaskFSRCNNDesc(FSRCNNDesc fsrcnn_desc,
+				   unsigned int block_size) :
+				   fsrcnn_desc(fsrcnn_desc), block_size(block_size) {}
 
 	TaskFSRCNNDesc(std::vector<unsigned short> kernels,
 				   std::vector<unsigned short> paddings,
 				   std::vector<unsigned short> channels,
 				   unsigned int block_size) :
-				   kernels(kernels), paddings(paddings), channels(channels), block_size(block_size) {}
+				   fsrcnn_desc(kernels, paddings, channels), block_size(block_size) {}
+
+	TaskKind task_kind() const override {
+		return TaskKind::fsrcnn;
+	}
 };
